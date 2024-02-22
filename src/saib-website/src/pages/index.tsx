3baa279a4429895f0604a5b7d417edc4141633b6
Alphonzo Escolar <phonz.dev@gmail.com>
2024-02-23 02:29:51 +0800

    implement responsive design

--- src/saib-website/src/pages/index.tsx
+++ src/saib-website/src/pages/index.tsx
@@ -1,21 +1,36 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import { Button, IconButton } from "@mui/material"
+import MenuIcon from '@mui/icons-material/Menu';
+
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4F37EA'
+    },
+    secondary: {
+      main: '#D714E8'
+    },
+  },
+});
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <div className="">
+    <div className="px-6">
       <div className="max-w-[1200px] m-auto mt-[24px]">
         <header className="flex justify-between text-white p-[24px] bg-[#1010108F] border-[1px] border-solid border-transparent rounded-[24px] backdrop-blur-md shadow-[0_4px_21px_0_#00000040]">
           <div className="flex gap-2 items-center">
-            <div>
+            <div className="w-[40px] md:w-max">
               <StaticImage src="../images/saib-logo.png" alt="SAIB logo" />
             </div>
-            <div>
+            <div className="hidden md:block">
               <StaticImage src="../images/saib.png" alt="SAIB text" />
             </div>
           </div>
-          <ul className="flex gap-6 items-center text-[18px]">
+          <ul className="flex gap-6 items-center text-[18px] hidden xl:flex">
             <li>
               <a href="#">About</a>
             </li>
@@ -26,21 +41,31 @@ const IndexPage: React.FC<PageProps> = () => {
               <a href="#">Integrations</a>
             </li>
           </ul>
-          <button className="text-[18px] bg-[#4F37EA] py-[12px] px-[24px] rounded-[50px] border-[1px] border-solid border-transparent">Get Started</button>
+          <button className="hidden xl:block text-[18px] bg-[#4F37EA] py-[12px] px-[24px] rounded-[50px] border-[1px] border-solid border-transparent">Get Started</button>
+          <div className="xl:hidden">
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+          </div>
         </header>
 
-        <main>
-          <div className="w-[630px] m-auto mt-[169px] text-center text-white">
-            <h1 className="font-bold text-[120px] leading-[120px]">Softwarez, <span className="font-normal font-['Poppins']">at its</span> <span className="text-[#4F37EA]">best</span><span className="font-sans">.</span></h1>
+        <main className="mt-16 md:mt-[180px] xl:mt-[169px]">
+          <div className="max-w-[630px] m-auto text-center text-white">
+            <h1 className="font-bold text-[55px] md:text-[105px] xl:text-[120px] leading-[55px] md:leading-[105px] xl:leading-[120px]">Softwarez, <span className="font-normal font-['Poppins']">at its</span> <span className="text-[#4F37EA]">best</span><span className="font-sans">.</span></h1>
             <p className="mt-6 leading-[27px] text-[18px] px-4">We are a software development company based in Cebu, Philippines that has been established through connecting the dots of its founders' shared and common passion for Cardano.</p>
             <button className="text-[18px] bg-[#4F37EA] py-[12px] px-[24px] rounded-[50px] border-[1px] border-solid border-transparent mt-6">Let's talk</button>
           </div>
 
-          <div className="mt-[226px] text-white p-[80px] rounded-[24px] bg-[#1010108F] border-[1px] border-solid border-transparent backdrop-blur-md shadow-[0_4px_21px_0_#00000040]">
-            <h2 className="text-[52px] font-bold leading-[62px] text-center">What we can do for you</h2>
+          <div className="mt-[130px] xl:mt-[226px] text-white py-16 px-2 xl:p-[80px] rounded-[24px] bg-[#1010108F] border-[1px] border-solid border-transparent backdrop-blur-md shadow-[0_4px_21px_0_#00000040]">
+            <h2 className="text-[36px] leading-[45px] md:text-[52px] font-bold xl:leading-[62px] text-center">What we can do for you</h2>
 
-            <div className="grid grid-cols-3 gap-10 mt-16">
-              <div className="flex items-center gap-3">
+            <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-10 mt-[45px] md:mt-[75px] xl:mt-16">
+              <div className="flex items-center gap-3 px-4">
                 <div className="w-[64px]">
                   <StaticImage src="../images/frontend.png" alt="" />
                 </div>
@@ -50,7 +75,7 @@ const IndexPage: React.FC<PageProps> = () => {
                 </div>
               </div>
 
-              <div className="flex items-center gap-3">
+              <div className="flex items-center gap-3 px-4">
                 <div className="w-[64px]">
                   <StaticImage src="../images/backend.png" alt="" />
                 </div>
@@ -60,7 +85,7 @@ const IndexPage: React.FC<PageProps> = () => {
                 </div>
               </div>
 
-              <div className="flex items-center gap-3">
+              <div className="flex items-center gap-3 px-4">
                 <div className="w-[64px]">
                   <StaticImage src="../images/software-development.png" alt="" />
                 </div>
@@ -70,7 +95,7 @@ const IndexPage: React.FC<PageProps> = () => {
                 </div>
               </div>
 
-              <div className="flex items-center gap-3">
+              <div className="flex items-center gap-3 px-4">
                 <div className="w-[64px]">
                   <StaticImage src="../images/blockchain.png" alt="" />
                 </div>
@@ -80,7 +105,7 @@ const IndexPage: React.FC<PageProps> = () => {
                 </div>
               </div>
 
-              <div className="flex items-center gap-3">
+              <div className="flex items-center gap-3 px-4">
                 <div className="w-[64px]">
                   <StaticImage src="../images/smart-contract.png" alt="" />
                 </div>
@@ -94,8 +119,8 @@ const IndexPage: React.FC<PageProps> = () => {
         </main>
       </div>
     </div>
-    
-  )
+
+  );
 }
 
 export default IndexPage
