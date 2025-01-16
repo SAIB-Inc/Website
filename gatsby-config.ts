511b6e02b2ff2c78559ed7f55d23f42c184da639
KeeeeEf <kfrgallego@gmail.com>
2025-01-17 00:45:51 +0800

    setting up configurations

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `SAIB Website`,
+    siteUrl: `https://saib.dev/`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
