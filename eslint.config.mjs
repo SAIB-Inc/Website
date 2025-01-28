c119b69c6c91936fdf14c342d74b8168e41e0715
KeeeeEf <kfrgallego@gmail.com>
2025-01-28 22:58:40 +0800

    feat: added and configured eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  { ignores: [".cache", "gatsby-config.ts", "public/~partytown/**", "src/gatsby-types.d.ts"] },
+  {
+    rules: {
+      "no-duplicate-imports": ["error", { "includeExports": true }],
+      "no-useless-assignment": "error"
+    }
+  }
+];
